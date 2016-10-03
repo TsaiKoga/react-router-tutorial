e019f7f3115342d5beaaccfa1b3b1d64d2706d73
TsaiKoga <koga.c@shopperplus.com>
2016-10-03 16:26:33 +0800

    Finish lesson 6:通过this.props.params得到链接参数

--- lessons/06-params/modules/NavLink.js
+++ lessons/06-params/modules/NavLink.js
@@ -4,6 +4,14 @@ import { Link } from 'react-router'
 
 export default React.createClass({
   render() {
-    return <Link {...this.props} activeClassName="active"/>
+    return (
+      <div>
+        <h2>Repo</h2>
+        <ul>
+          <li><Link to="/repos/reactjs/react-router">React Router</Link></li>
+          <li><Link to="/repos/facebook/reactjs">ReactJs</Link></li>
+        </ul>
+      </div>
+    )
   }
 })
